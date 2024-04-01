6121de67ac9cbcc3d435b87b68f753846925f65c
LENGKEA-DEVELOPER <LENGKEA-DEVELOPER>
2024-04-01 18:09:42 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Application from './APIs/GET_index';
+const App = () => {
+    return (
+        <>
+            <Application/>
+        </>
+    );
+};
+export default App;
---
+++ src/components/organisms/barloading/index.jsx
@@ -0,0 +1,14 @@
+import '../../../styles/cart.css'
+const Barloading = () => {
+    return(
+        <>
+        <div className="barloading">
+            <div className="box-loading">
+
+            </div>
+        </div>
+        
+        </>
+    )
+}
+export default Barloading;
